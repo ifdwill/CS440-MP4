0a8cbca037173a21f08980a3b73e7ef06513cd6d
William Zhang <tis.william@gmail.com>
2019-04-17 16:24:09 -0500

    minibatch_gd(), test_nn(), four_nn() added

--- part 2/neural_network.py
+++ part 2/neural_network.py
@@ -24,7 +24,27 @@ import numpy as np
 def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_classes, shuffle=True):
 
     #IMPLEMENT HERE
-    losses = 0
+    losses = []
+    for i in range(epoch):
+        print("Epoch %d" % i)
+        secondrange = len(x_train)/200
+        losstotal = 0
+        for i in range(secondrange):
+            if shuffle :
+                state = np.random.get_state()
+                np.random.shuffle(x_train)
+                np.random.set_state(state)
+                np.random.shuffle(y_train)
+                X = x_train[0:200]
+                y = y_train[0:200]
+                
+            else :
+                X = x_train[i*200:(i+1)*200]
+                y = y_train[i*200:(i+1)*200]
+            losstemp, w1, w2, w3, w4, b1, b2, b3, b4 = four_nn(X, w1, w2, w3, w4, b1, b2, b3, b4, y, test=False)
+            losstotal+=losstemp
+        #print(losstemp)
+        losses.append(losstotal)
     return w1, w2, w3, w4, b1, b2, b3, b4, losses
 
 """
@@ -47,6 +67,17 @@ def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
 
     avg_class_rate = 0.0
     class_rate_per_class = [0.0] * num_classes
+    classification = np.zeros(len(x_test))
+    #print(len(x_test))
+    
+    classification = four_nn(x_test, w1, w2, w3, w4, b1, b2, b3, b4, y_test, True)
+    avg_class_rate = (classification == y_test).mean()
+    y_unique, y_counts = np.unique(y_test, return_counts=True)
+    for i in range(len(x_test)) :
+        if classification[i] == y_test[i] :
+            class_rate_per_class[classification[i]] += 1
+    for i in range(num_classes):
+        class_rate_per_class[i] /= y_counts[i]
     return avg_class_rate, class_rate_per_class
 
 """
@@ -55,8 +86,42 @@ def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
     Up to you on how to implement this, won't be unit tested
     Should call helper functions below
 """
-def four_nn():
-    pass
+def four_nn(X, w1, w2, w3, w4, b1, b2, b3, b4, y, test):
+    
+    Z1, acache1 = affine_forward(X, w1, b1)
+    A1, rcache1 = relu_forward(Z1)
+    Z2, acache2 = affine_forward(A1, w2, b2)
+    A2, rcache2 = relu_forward(Z2)
+    Z3, acache3 = affine_forward(A2, w3, b3)
+    A3, rcache3 = relu_forward(Z3)
+    F,  acache4 = affine_forward(A3, w4, b4)
+
+    if test == True:
+        classifications = []
+        for i in range(len(F)) :
+            classifications.append(np.argmax(F[i]))
+        #print(len(classifications))
+        return classifications
+
+    loss, dF = cross_entropy(F, y)
+
+    dA3, dw4, db4 = affine_backward(dF,  acache4)
+    dZ3 = relu_backward(dA3, rcache3)
+    dA2, dw3, db3 = affine_backward(dZ3, acache3)
+    dZ2 = relu_backward(dA2, rcache2)
+    dA1, dw2, db2 = affine_backward(dZ2, acache2)
+    dZ1 = relu_backward(dA1, rcache1)
+    dX, dw1, db1  = affine_backward(dZ1, acache1)
+    w1 -= (0.1)*dw1
+    w2 -= (0.1)*dw2
+    w3 -= (0.1)*dw3
+    w4 -= (0.1)*dw4
+    b1 -= (0.1)*db1
+    b2 -= (0.1)*db2
+    b3 -= (0.1)*db3
+    b4 -= (0.1)*db4
+
+    return loss, w1, w2, w3, w4, b1, b2, b3, b4
 
 """
     Next five functions will be used in four_nn() as helper functions.
